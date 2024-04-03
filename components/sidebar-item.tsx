17c557f03b3ef745a74f2d93c96dd9524f0fd3d4
gustavogss <gustavogss.dev@gmail.com>
2024-04-03 19:36:03 -0300

    feat: side-bar create

---
+++ components/sidebar-item.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+type Props = {
+  label: string;
+  icon: string;
+  href: string;
+};
+
+export const SidebarItem = ({
+  label,
+  icon,
+  href,
+}: Props) => {
+  const pathname = usePathname();
+  const active = pathname === href;
+
+  return (
+    <Button
+      variant={active ? "sidebarOutline" : "sidebar"}
+      className="justify-start h-[52px]"
+      asChild
+    >
+      <Link href={href}>
+        <Image
+          src={icon}
+          alt={label}
+          className="mr-5"
+          height={32}
+          width={32}
+        />
+        {label}
+      </Link>
+    </Button>
+  );
+};

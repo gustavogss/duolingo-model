7cb7e55d096785118f816d453a63aaece6714e3c
gustavogss <gustavogss.dev@gmail.com>
2024-06-01 17:11:21 -0300

    chore: drizzle update

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -25,14 +25,34 @@ export function Quiz({
 
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex((challenge) => !challenge.completed);
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+
+  const challenge = challenges[activeIndex];
+
+  const title = challenge.type === 'ASSIST' ? "Select the correct meaming"
+    : challenge.question;
 
   return (
-    <div className="">
+    <>
       <Header
         hearts={hearts}
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
-    </div>
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+            <h1 className="tex-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div className=""></div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
